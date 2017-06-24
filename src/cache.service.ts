4f9a893b11bbebadad05695d3327bee6b1cb36fb
thebarada <themrbarada@gmail.com>
2017-06-25 02:29:29 +0300

    feat(cache service) remove all extraneous logic from service

--- src/cache.service.ts
+++ src/cache.service.ts
@@ -1,25 +1,8 @@
-import {
-  Inject, Injectable, InjectionToken, Injector, PLATFORM_ID, RendererFactory2,
-  ViewEncapsulation
-} from '@angular/core';
-import { PlatformState } from '@angular/platform-server';
-import { isPlatformBrowser } from '@angular/common';
-
-export const APP_CACHE_KEY = new InjectionToken('APP_CACHE_KEY');
+import { Injectable } from '@angular/core';
 
 @Injectable()
 export class CacheService {
   private cache: { [key: string]: any } = {};
-
-  constructor(@Inject(PLATFORM_ID) private platformId: any,
-              @Inject(APP_CACHE_KEY) private appCacheKey: string,
-              private injector: Injector,
-              private rendererFactory: RendererFactory2) {
-    if (isPlatformBrowser(this.platformId)) {
-      const serverCache = rehydrateCache(appCacheKey, this.cache);
-      this.rehydrate(serverCache);
-    }
-  }
 
   has(key: string): boolean {
     return key in this.cache;
@@ -51,57 +34,13 @@ export class CacheService {
     return json;
   }
 
+  toJSON(): { [key: string]: any } {
+    return this.dehydrate();
+  }
+
   rehydrate(json: { [key: string]: any }): void {
     Object.keys(json).forEach((key: string) => {
       this.cache[key] = json[key];
     });
   }
-
-  toJSON(): { [key: string]: any } {
-    return this.dehydrate();
-  }
-
-  inject(): void {
-    try {
-      const platformState = this.injector.get(PlatformState);
-      const document: any = platformState.getDocument();
-      const state = JSON.stringify(this.toJSON());
-      const renderer = this.rendererFactory.createRenderer(document, {
-        id: '-1',
-        encapsulation: ViewEncapsulation.None,
-        styles: [],
-        data: {}
-      });
-
-      const html: any = Array.from(document.children).find((child: any) => child.name === 'html');
-      const head = Array.from(html.children).find((child: any) => child.name === 'head');
-
-      if (!head) {
-        throw new Error('<head> not found in the document');
-      }
-
-      const script = renderer.createElement('script');
-      renderer.setValue(script, `window['${this.appCacheKey}'] = ${state}`);
-      renderer.appendChild(head, script);
-    } catch (e) {
-      console.error(e);
-    }
-  }
-}
-
-export function rehydrateCache(appCacheKey: string, defaultValue: { [key: string]: any }): { [key: string]: any } {
-  const win: any = window;
-  if (win[appCacheKey]) {
-    let serverCache = defaultValue;
-    try {
-      serverCache = JSON.parse(JSON.stringify(win[appCacheKey]));
-      if (typeof serverCache !== typeof defaultValue) {
-        serverCache = defaultValue;
-      }
-    } catch (e) {
-      serverCache = defaultValue;
-    }
-    return serverCache;
-  }
-  return defaultValue;
 }
